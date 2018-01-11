69a58d7c87a2ece6325d4cfeeb326486d89c73ab
Berk Çebi <berk@zeplin.io>
2018-01-11 09:58:46 +0300

    Tweak CSS built-in case

--- components/prism-css-extras.js
+++ components/prism-css-extras.js
@@ -1,5 +1,5 @@
 Prism.languages.insertBefore("css", "function", {
-    builtin: /\b(thin|medium|thick|hidden|dotted|dashed|solid|double|groove|ridge|inset|outset|to|left|right|top|bottom|normal|bold|bolder|lighter|start|end|center|justify|match-parent)/i,
+    builtin: /\b(thin|medium|thick|hidden|dotted|dashed|solid|double|groove|ridge|inset|outset|to|left|right|top|bottom|normal|bold|bolder|lighter|start|end|center|justify|match-parent)/,
     hexcode: /#[\da-f]{3,8}/i,
     // Add units.
     number: /-?\d+(\.\d+)?(%|em|ex|ch|rem|vw|vh|vmin|vmax|cm|mm|in|pt|pc|px|deg|grad|rad|turn|s|ms|Hz|kHz|dpi|dpcm|dppx)?/i
