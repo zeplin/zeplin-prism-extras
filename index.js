a23164668c97540659315cfcb76b58ad1165b190
Berk Çebi <berk@zeplin.io>
2018-01-11 13:46:13 +0300

    Add markup extras

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ import "./components/prism-less-extras";
 import "./components/prism-stylus-extras";
 import "./components/prism-swift-extras";
 import "./components/prism-objectivec-extras";
+import "./components/prism-markup-extras";
