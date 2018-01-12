3a4debd3d92b454025e332c26b3bd27e384c771b
Berk Çebi <berk@zeplin.io>
2018-01-12 17:55:31 +0300

    Prefer `basename`

--- uglify.sh
+++ uglify.sh
@@ -7,8 +7,7 @@ DIR="dist"
 mkdir -p $DIR
 
 for COMPONENT_PATH in components/*.js; do
-    COMPONENT=${COMPONENT_PATH#$COMPONENTS_DIR/}
-    COMPONENT_NAME=${COMPONENT%.*}
+    COMPONENT_NAME=$(basename $COMPONENT_PATH .js)
 
     ./node_modules/.bin/uglifyjs $COMPONENT_PATH -o $DIR/$COMPONENT_NAME.min.js --source-map filename=$DIR/$COMPONENT_NAME.min.js.map
 done
